30d292bbe5645ed0306f66317325bf79161bd73e
Ollebogge <obogardh@gmail.com>
2025-03-24 11:21:03 +0100

    random save

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,34 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Products from './views/Products.jsx'
+import ProductDetail from './views/ProductDetail.jsx'
+import Home from './views/Home.jsx'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {path: "/",
+     element: <App />, 
+    children:[
+      {
+      path: "/", 
+      element: <Home />
+    },
+    {
+      path: "products/all",
+      element: <Products />
+    },
+    {
+      path: "products/detail",
+      element: <ProductDetail/>
+    },
+    
+  ] 
+}]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
+ 
