a814ec43eb386b789089976dc97a56f0f972824e
Eriktysklind <h23eribl@du.se>
2025-03-24 09:09:23 +0100

    Tillägg av views med mera

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Products from './views/Products.jsx'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {path: "/", element: <App />},
+  {path: "/products/all", element: <Products/>} ]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
