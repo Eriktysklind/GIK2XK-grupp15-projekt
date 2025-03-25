698fee07f23c4f6c8d99ac009e5272dee90ce392
Davidskaring <h23davsk@du.se>
2025-03-25 09:22:32 +0100

    ändrade till products i app js samt get all is productService

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 ///app.use(express.static(path.join(__dirname, 'public')));
-app.use("/product", require("./routes/productRoute"));
+app.use("/products", require("./routes/productRoute"));
 app.use("/users", require("./routes/userRoute"));
 
 module.exports = app;
