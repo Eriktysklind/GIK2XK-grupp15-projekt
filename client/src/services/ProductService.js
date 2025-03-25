1a6c135a7fb755003b5f7c5357b6718fa42ef653
Davidskaring <h23davsk@du.se>
2025-03-25 01:24:19 +0100

    fix med bin och services

---
+++ client/src/services/ProductService.js
@@ -0,0 +1,80 @@
+import axios from './api';
+
+export async function getAll(endpoint = '/product') {
+    try {
+        const response = await axios.get(endpoint);
+    
+        if(response.status === 200) return response.data;
+        else{
+            console.log(response)
+            return [];
+        }
+    } catch(e) {
+        e?.response ? console.log(e.response.data) : console.log(e);
+    }
+ }
+
+ export async function getOne(id){
+    try {
+        const response = await axios.get(`/product/${id}`);
+        if (response.status === 200) return response.data;
+        else {
+          console.log(response.data);
+          return null;
+        }
+    } catch(e){
+        e?.response ? console.log(e.response.data) : console.log(e);
+    }
+ }
+
+ export async function create(product) {
+    try {
+      const response = await axios.post('/product', product);
+      if (response.status === 200) return response.data;
+      else {
+        console.log(response.data);
+        return null;
+      }
+    } catch (e) {
+      e?.response ? console.log(e.response.data) : console.log(e);
+    }
+  }
+
+  export async function update(product) {
+    try {
+      const response = await axios.put('/product', product);
+      if (response.status === 200) return response.data;
+      else {
+        console.log(response.data);
+        return null;
+      }
+    } catch (e) {
+      e?.response ? console.log(e.response.data) : console.log(e);
+    }
+  }
+
+ /* export async function remove(id) {
+    try {
+      const response = await axios.delete('/product', { data: { id } });
+      if (response.status === 200) return response.data;
+      else {
+        console.log(data);
+        return null;
+      }
+    } catch (e) {
+      e?.response ? console.log(e.response.data) : console.log(e);
+    }
+  }
+
+  export async function addRating(productId, rating) {
+    try {
+      const response = await axios.post(`/product/${productId}/addRating`, rating);
+      if (response.status === 200) return response.data;
+      else {
+        console.log(data);
+        return null;
+      }
+    } catch (e) {
+      e?.response ? console.log(e.response.data) : console.log(e);
+    }
+  } */
