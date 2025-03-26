16ea03c22f9be9f15572e399f36c7bfc275a3f34
Eriktysklind <h23eribl@du.se>
2025-03-26 12:14:23 +0100

    Footer tillagd

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import {  Outlet } from "react-router-dom"
 import { Box, Container} from '@mui/material';
 import ResponsiveAppBar from "./componets/ReponsiveAppBar";
+import Footer from "./componets/Footer";
 
 
 function App() {
@@ -10,14 +11,14 @@ function App() {
     <Box sx={{
       bgcolor: "#f5f5f5",
       minHeight: "100vh",
-      
     }}>
     <ResponsiveAppBar />
-    
     <Container sx={{mt: 4}} maxWidth="xl" component="main">
       <Outlet/>
       </Container>
+      <Footer/>
       </Box>
+      
     </>
   )
 }
---
+++ client/src/componets/Footer.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Box, Container, Typography, Link, Stack } from '@mui/material';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import ikon from '../assets/loggotyp.png';
+
+
+const Footer = () => {
+  return (
+    <Box sx={{ backgroundColor: '#e3f2fd', color: 'black', py: 4}}>
+      <Container maxWidth="lg">
+        <Stack direction={{ xs: 'column', md: 'row'}} justifyContent={"space-between"} spacing={1}>
+          <Box>
+            <Typography variant="subtitle1" gutterBottom>Kontakt</Typography>
+            <Typography variant="body2">Email: Namn@mittforetag.com</Typography>
+            <Typography variant="body2">Telefon: 070-123 45 67</Typography>
+          </Box>
+          <Box>
+            <Typography variant="subtitle1" gutterBottom>Följ oss</Typography>
+            <Link href="https://www.instagram.com/ollebogardhdg/" color="inherit" underline="hover"><InstagramIcon fontSize="small" /></Link>
+            <Link href="#" color="inherit" underline="hover" style={{padding:"1rem"}}><FacebookIcon fontSize="small" /></Link>
+          </Box>
+          <Box
+            component="img"
+            src={ikon}
+            alt='Logga'
+            textAlign='center'
+            sx={{height: 100, width: 100, ml: 'auto', borderRadius: 5}} 
+          />
+        </Stack>
+      </Container>
+    </Box>
+  );
+};
+
+export default Footer;
